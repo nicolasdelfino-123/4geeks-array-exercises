1eb26de80b97c9da5baf04350b1d4cd6ced096cd
Nicolas <nicolasdelfino585@gmail.com>
2024-10-16 14:29:16 -0300

    hasta el 11 done

--- exercises/11-Do-while/app.js
+++ exercises/11-Do-while/app.js
@@ -1,7 +1,22 @@
+/*Imprime cada número de la iteración en la consola del 20 al 0.
+
+Si el número es un múltiplo de 5, concaténale un signo de exclamación ! al elemento.
+
+Cuando el bucle llegue a cero, imprime LIFTOFF en vez del número 0. 
+Esta instrucción debe ir también dentro del bucle. */
+
 let i = 20;
 
 do {
     // Magic goes here
-
+    if (i === 0){
+        console.log("LIFTOFF");
+    } 
+    else if (i % 5 === 0){
+        console.log(i + "!");
+    }
+    else{
+        console.log(i)
+    }
     i--;
 } while (i >= 0);

34df725c31cf9e6a880d0b2f50107c42669ee2e4
Nicolas <nicolasdelfino585@gmail.com>
2024-10-24 12:50:45 -0300

    repasados ejercicios con dificultades por segunda vez

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,17 @@
+/* Usando un bucle for, invierte el array arr e imprime el nuevo array en la consola. */
+
 let arr = [45,67,87,23,5,32,60];
 
 // Your code here
+
+/* let arrayInverted = arr.reverse(arr);
+console.log(arrayInverted); */
+
+let arrayInverted = []
+
+for(let i = arr.length -1; i >= 0; i--){
+    let item = arr[i]
+    arrayInverted.push()
+
+}
+console.log(arrayInverted);
--- exercises/10-Everything-is-awesome/app.js
+++ exercises/10-Everything-is-awesome/app.js
@@ -1,9 +1,21 @@
+/*Compara el elemento. Si es 1, agrega el número en el arreglo return_array.
+
+Compara el elemento. Si es 0, agrega el string 'Yahoo' en el arreglo return_array (en lugar del número).
+
+Por ejemplo, la salida de [0,0,1,1,0] sería: */
+
 let myArray = [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 1 ];
 
 const ZerosToYahoos = (arr) => {
     let return_array = [];
     arr.forEach((item) => {
             // magic goes inside these brackets
+            if(item === 1){
+                return return_array.push(item)
+            }
+            else{
+                return return_array.push("Yahoo")
+            }
     });
     return return_array;
 };

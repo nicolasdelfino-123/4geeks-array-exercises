35f63b54ccc6a2c1e619528f0115e9d23c3be9a3
Nicolas <nicolasdelfino585@gmail.com>
2024-10-24 18:58:33 -0300

    ejercicio 17 repasado ok

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,20 +1,20 @@
-/*Instrucciones:
+/*📝 Instrucciones:
 Calcula el valor promedio de todos los elementos del arreglo.
-
 Imprímelo en la consola.
-
 💡 Pistas:
 Recuerda utilizar variables auxiliares.
-
 Para obtener el promedio, debes sumar todos los valores y dividir el resultado por la longitud total del arreglo. */
 
 let myArray = [2323, 4344, 2325, 324413, 21234, 24531, 2123, 42234, 544, 456, 345, 42, 5445, 23, 5656, 423];
 
 // Your code here
-let total = 0
 
-for (let i of myArray){
-    total += i
+let total = 0
+let items = myArray.length
+for(let item of myArray){
+    total += item
 }
-let promedio = total / myArray.length
-console.log(promedio)
+
+let promedio = total / items
+
+console.log(promedio);

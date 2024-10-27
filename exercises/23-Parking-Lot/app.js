cd806eb511317d8f2173d39bd63917b8f1499354
Nicolas <nicolasdelfino585@gmail.com>
2024-10-27 16:27:47 -0300

    ejercicio 24 done

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -1,3 +1,9 @@
+/*Instrucciones:
+Crea una función getParkingLotState() que devuelva un objeto con totalSlots (cantidad 
+total de puestos), availableSlots 2 (puestos disponibles) y occupiedSlots 1(puestos ocupados) 
+de esta forma: 
+USAR BUCLE ANIDADO*/
+
 let parkingState = [
   [1, 0, 1, 1],
   [0, 0, 0, 2],
@@ -6,6 +12,25 @@ let parkingState = [
 ]
 
 // Your code here
+function getParkingLotState(array){
+  let resultado = {
+    ocupado: 0,
+    disponible: 0,
+    total: 0
+  }
+    for(let i = 0; i < array.length;i++){
+      for(let x = 0; x < array[i].length;x++){
+        if(array[i][x] === 2){
+          resultado.disponible += 1
+          resultado.total += 1
+        }
+        else if( array[i][x] === 1){
+          resultado.ocupado += 1
+          resultado.total += 1
+        }
+      }
+    }
+    return resultado
 
-
-console.log(getParkingLotState(parkingState))
+}
+console.log(getParkingLotState(parkingState));
